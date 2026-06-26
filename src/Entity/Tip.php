a95fb5ea9c616da6c7d0d85ce7ded1c772997cb8
Loïc PILLER <loicpiller@mail.fr>
2026-06-26 23:04:44 +0200

    feat: add Tip entity and migration

---
+++ src/Entity/Tip.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TipRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TipRepository::class)]
+class Tip
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $text = null;
+
+    #[ORM\Column]
+    private array $months = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): static
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getMonths(): array
+    {
+        return $this->months;
+    }
+
+    public function setMonths(array $months): static
+    {
+        $this->months = $months;
+
+        return $this;
+    }
+}
